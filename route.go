2afd62410609e12f38053c63f3506d388c8f70b8
Mark Ehresman <435089@gmail.com>
2018-03-31 13:17:59 -0500

    Finished initial development

--- route.go
+++ route.go
@@ -1,32 +1,83 @@
 package rest
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type InternalRoute struct {
+	Description string
+	Method      string
+	Path        string
+	f           http.HandlerFunc
+}
+
 type Route struct {
 	Description string
 	Method      string
 	Path        string
-	f           func() interface{}
+	f           func() (interface{}, error)
 }
 
 type Routes []Route
 
-type Router mux.Router
+type DataWrapper struct {
+	Data interface{} `json:"data"`
+}
+
+type ErrorWrapper struct {
+	Error error `json:"error"`
+}
+
+type InternalRoutes []InternalRoute
+
+type Router struct {
+	muxRouter *mux.Router
+}
+
+func (this *Router) Start(port string) {
+	http.ListenAndServe(port, this.muxRouter)
+}
 
 func NewRouter(routes Routes) *Router {
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
 		var handler http.Handler
-		handler = route.HandlerFunc
-		handler = Logger(handler, route.Name)
+		handler = NewHandlerFunc(route.f)
+		//handler = Logger(handler, route.Name) Use this method of 'logging' later for user authentication
 		router.
 			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
+			Path(route.Path).
+			Name(route.Description).
 			Handler(handler)
+	}
+	return &Router{
+		muxRouter: router,
+	}
+}
+
+func NewHandlerFunc(f func() (interface{}, error)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		result, err := f()
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		data := &DataWrapper{
+			Data: result,
+		}
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			error_string := &ErrorWrapper{
+				Error: err,
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			if err := json.NewEncoder(w).Encode(error_string); err != nil {
+				panic(err)
+			}
+		}
 	}
 }
