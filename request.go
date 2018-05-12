f2d1ff87b54892c917f85e306031d782ad306139
Mark Ehresman <435089@gmail.com>
2018-05-12 16:32:00 -0500

    added query params, now finishing debugging.

--- request.go
+++ request.go
@@ -4,13 +4,15 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 
 	"github.com/gorilla/mux"
 )
 
 type Request struct {
-	Vars map[string]string
-	Body []byte
+	Vars   map[string]string
+	Body   []byte
+	Params map[string]string
 }
 
 func NewRequest(r *http.Request) (*Request, error) {
@@ -21,8 +23,19 @@ func NewRequest(r *http.Request) (*Request, error) {
 	if err := r.Body.Close(); err != nil {
 		return nil, err
 	}
+	queries := make(map[string]string)
+	query_map := r.URL.Query()
+	for index, value := range query_map {
+		param, err := url.QueryUnescape(value[0])
+		if err != nil {
+			queries[index] = value[0]
+			continue
+		}
+		queries[index] = param
+	}
 	return &Request{
-		Vars: mux.Vars(r),
-		Body: body,
+		Vars:   mux.Vars(r),
+		Body:   body,
+		Params: queries,
 	}, nil
 }
